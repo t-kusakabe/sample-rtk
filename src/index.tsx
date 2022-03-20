958e4c5b54c501b45caa89ac6736fa36884e670c
t-kusakabe <ttmac0704@gmail.com>
2022-03-20 19:23:10 +0900

    mockServerを用意しておく

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,34 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { rest, setupWorker } from 'msw';
+
+const mockServer = setupWorker(
+  rest.post('/login', (req, res, ctx) => {
+    const { userName }: any = req.body;
+
+    if (userName === 'john') {
+      return res(
+        ctx.delay(2000),
+        ctx.status(200),
+        ctx.json({
+          userName: 'john',
+          token: 'token1234'
+        })
+      );
+    }
+
+    return res(
+      ctx.delay(2000),
+      ctx.status(401),
+      ctx.json({
+        message: 'unauthorized'
+      })
+    );
+  })
+);
+
+mockServer.start();
 
 ReactDOM.render(
   <React.StrictMode>
