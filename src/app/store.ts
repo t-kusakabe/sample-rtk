7c789d686b425236126d67fe917dae96757f3866
t-kusakabe <ttmac0704@gmail.com>
2022-03-20 19:57:55 +0900

    auth周りをstoreに含める

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,16 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import authReducer from '../features/auth/authSlice';
+import rtkAuthReducer, { authApi } from '../features/rtk-auth/rtkAuthSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    auth: authReducer,
+    rtkauth: rtkAuthReducer,
+    [authApi.reducerPath]: authApi.reducer
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware)
 });
 
 export type AppDispatch = typeof store.dispatch;
